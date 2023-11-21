47f8b0d87cddc6ad662a7064275c0923d0466990
2P1F <60047882+HellZEras@users.noreply.github.com>
2023-11-21 12:08:24 +0100

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -4,34 +4,44 @@ import os
 import json
 import psutil
 
-CLIENT_ID = "replace with ur client id"
-redirect_uri = "replace with ur redirect uri"
-CLIENT_SECRET = "replace with ur client secret key" 
-username = "replace with ur username"
+CLIENT_ID = "a1aec2f25ad2471c8bee8fa44acc204c"
+redirect_uri = "http://localhost:8888/callback"
+CLIENT_SECRET = "145348ab4ef649188c7cf3934023295a"
+username = "2P1F"
 scope = "user-read-currently-playing"
-token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
-sp = spotipy.Spotify(auth=token)
-var = False
-data = {}
-def is_process_running(name):
+
+def get_spotify_token():
+    token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
+    return token
+
+def is_spotify_online():
     for process in psutil.process_iter(['name']):
-        if process.info['name'] == name:
-            return "Spotify Online"
-    return "Spotify Offline"
-if is_process_running("spotify") == "Spotify Online":
-    data['text'] = "Spotify Online"
-    var = True
+        if process.info['name'] == "spotify":
+            return True
+    return False
 
-
-if var:    
+def get_current_song(sp):
     try:
-        currentsong = sp.currently_playing()
-        song_name = currentsong['item']['name']
-        song_artist = currentsong['item']['artists'][0]['name']
-        data['text'] = "🎵 {} by {}".format(song_name, song_artist)
+        current_song = sp.currently_playing()
+        song_name = current_song['item']['name']
+        song_artist = current_song['item']['artists'][0]['name']
+        return "🎵 {} by {}".format(song_name, song_artist)
     except Exception:
-        data['text'] = "Spotify Online"
-else:
-    data['text'] = "Spotify Offline"
-print(json.dumps(data))
+        return "Spotify Online"
 
+def main():
+    token = get_spotify_token()
+    sp = spotipy.Spotify(auth=token)
+
+    if is_spotify_online():
+        try:
+            data = {'text': get_current_song(sp)}
+        except Exception as e:
+            data = {'text': "Error: {}".format(str(e))}
+    else:
+        data = {'text': "Spotify Offline"}
+
+    print(json.dumps(data))
+
+if __name__ == "__main__":
+    main()
