410419ae2161db76809f391a204628e138dee4d9
2P1F <60047882+HellZEras@users.noreply.github.com>
2024-10-02 16:11:52 +0100

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -1,46 +1,48 @@
-import spotipy
-import spotipy.util as util
-import os
+import subprocess
 import json
-import psutil
 
-CLIENT_ID = "Replace with ur client id"
-redirect_uri = "Replace with ur redirect uri"
-CLIENT_SECRET = "Replace with ur client secret"
-username = "Replace with ur username"
-scope = "user-read-currently-playing"
+def spotify_status():
+    result = subprocess.run(["playerctl", "--player=spotify", "status"], capture_output=True, text=True)
+    result = result.stdout.strip()
+    if result == "Paused":
+        return "Paused"
+    elif result == "No players found":
+        return "Not Found"
+    elif result == "Playing":
+        return "Playing"
+    else:
+        return "Unknown"
 
-def get_spotify_token():
-    token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
-    return token
+def parse_spotify_data(data):
+    parsed_data = {}
+    artist, title = None, None  # Initialize artist and title
+    
+    lines = data.splitlines()
+    for line in lines:
+        if "artist" in line:
+            index = line.find("artist") + len("artist")
+            artist = line[index:].strip()
+        elif "title" in line:
+            index = line.find("title") + len("title")
+            title = line[index:].strip()
 
-def is_spotify_online():
-    for process in psutil.process_iter(['name']):
-        if process.info['name'] == "spotify":
-            return True
-    return False
-
-def get_current_song(sp):
-    try:
-        current_song = sp.currently_playing()
-        song_name = current_song['item']['name']
-        song_artist = current_song['item']['artists'][0]['name']
-        return "🎵 {} by {}".format(song_name, song_artist)
-    except Exception:
-        return "Spotify Online"
+    if artist and title:
+        return f"{title} By {artist}"
+    else:
+        return "Unknown song"
 
 def main():
-    token = get_spotify_token()
-    sp = spotipy.Spotify(auth=token)
-
-    if is_spotify_online():
-        try:
-            data = {'text': get_current_song(sp)}
-        except Exception as e:
-            data = {'text': "Error: {}".format(str(e))}
+    status = spotify_status()
+    data = {}
+    if status == "Paused":
+        data = {'text': "Spotify Is Paused"}
+    elif status == "Playing":
+        result = subprocess.run(["playerctl", "--player=spotify", "metadata"], capture_output=True, text=True)
+        to_write = parse_spotify_data(result.stdout)
+        data = {'text': to_write}
     else:
-        data = {'text': "Spotify Offline"}
-
+        data = {'text': "Spotify Is Offline"}
+    
     print(json.dumps(data))
 
 if __name__ == "__main__":
